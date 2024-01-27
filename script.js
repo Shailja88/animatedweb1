41aa1c251576e78d30590d879ebe7fd3455ca560
Shailja <genieshailja@gmail.com>
2024-01-27 23:38:20 +0530

    🏌️ Sidcup Family Golf Clone ui
    Recreate the magic of Sidcup Family Golf with this immersive website clone! ⛳️ Dive into the world of golfing fun using HTML, CSS, and JavaScript, enhanced with smooth animations powered by GSAP. 🚀
    
    Key Features:
    
    🎨 Stunning design: Meticulously crafted UI/UX to mirror the charm of Sidcup Family Golf.
    ⚙️ Powered by GSAP: Elevate user experience with fluid animations and seamless transitions.
    🛠️ Tech Stack: HTML, CSS, JavaScript - the backbone of an engaging golfing experience.

---
+++ script.js
@@ -0,0 +1,117 @@
+var crsr=document.querySelector("#cursor");
+var blur=document.querySelector("#cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    crsr.style.left=dets.x+20+"px";
+    crsr.style.top=dets.y+"px"
+    blur.style.left=dets.x-150+"px";
+    blur.style.top=dets.y-150+"px"
+})
+
+
+ var h4all =document.querySelectorAll("#nav h4")
+ h4all.forEach(function(ele){
+   ele.addEventListener("mouseenter",function(){
+    crsr.style.scale=3;
+    crsr.style.border="0.6px solid #fff";
+    crsr.style.backgroundColor="transparent"
+
+   })
+
+   ele.addEventListener("mouseleave",function(){
+    crsr.style.scale=1;
+    crsr.style.border="0px solid #95C11E";
+    crsr.style.backgroundColor="#95C11E"
+
+   })
+ })
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height:"100px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+gsap.to("#main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        start:"top -25%",
+        end:"top -75%",
+        scrub:2
+    }
+})
+gsap.from("#about-us img,#about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+   
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+   stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+        scrub:1
+    }
+})
+gsap.from("#colon1",{
+ y:-70,
+ x:-70,
+ scrollTrigger:{
+    trigger:"#colon1",
+    scroller:"body",
+    // markers:true,
+    start:"top 55%",
+    end:"top 45%",
+    scrub:4
+
+ }
+})
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+       trigger:"#colon1",
+       scroller:"body",
+    //    markers:true,
+       start:"top 55%",
+       end:"top 45%",
+       scrub:4
+   
+    }
+   })
+   gsap.from("#page4 h1",{
+   y:60,
+   scrollTrigger:{
+    trigger:"#page4 h1",
+    scroller:"body",
+ //    markers:true,
+    start:"top 85%",
+    end:"top 70%",
+    scrub:3
+
+ }
+
+   })
